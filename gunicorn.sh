1eb7f99e311d898b761502e9d918b269a2593b45
MoHaMeDMaMdouHFaDel <00mansionmohamed00@gmail.com>
2024-04-21 00:24:39 +0200

    gunicorn add

---
+++ gunicorn.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source env/bin/activate
+
+cd  /var/lib/jenkins/workspace/django-cicd/app
+
+pyhton3 manage.py 
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "migration done"
+
+cd  /var/lib/jenkins/workspace/django-cicd
+
+sudo cp -rf gunicorn.socket /etc/systemd/system
+sudo cp -rf gunicorn.service /etc/systemd/system
+
+
+echo "$USER"
+echo "$PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo "Gunicorn has been start"
+
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
+
