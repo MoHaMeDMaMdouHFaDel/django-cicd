d192d80cb90656c6e6a9731698ec73b81f680549
MoHaMeDMaMdouHFaDel <00mansionmohamed00@gmail.com>
2024-04-21 22:23:04 +0200

    nginx,sh modified

--- nginx.sh
+++ nginx.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-sudo apt-get install nginx -y
+
 sudo cp -rf app.conf /etc/nginx/sites-available/app
-
 chmod 710 /var/lib/jenkins/workspace/django-cicd
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
-sudo nginx -t 
 
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo nginx -t
 
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
-echo "Nginx has been start"
+echo "Nginx has been started"
 
 sudo systemctl status nginx
